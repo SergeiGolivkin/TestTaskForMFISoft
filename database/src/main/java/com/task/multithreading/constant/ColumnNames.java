08e38caaf7d96466c2c7c3801b00df1313d7c7ae
Sergey <golivkin.s.v@gmail.com>
2021-08-15 09:48:38 +0300

    constant

---
+++ database/src/main/java/com/task/multithreading/constant/ColumnNames.java
@@ -0,0 +1,10 @@
+package com.task.multithreading.constant;
+
+public final class ColumnNames {
+
+    public static final String ID_COLUMN_LABEL = "id";
+    public static final String TITLE_COLUMN_LABEL = "title";
+    public static final String NEWS_SITE_COLUMN_LABEL = "news_site";
+    public static final String PUBLISHED_DATE_COLUMN_LABEL = "published_date";
+    public static final String ARTICLE_COLUMN_LABEL = "article";
+}
