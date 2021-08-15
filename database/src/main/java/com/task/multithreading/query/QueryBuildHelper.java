0bb2ad861cf35a5f7616950999cc96b9f9f0f494
Sergey <golivkin.s.v@gmail.com>
2021-08-15 09:50:54 +0300

    query

---
+++ database/src/main/java/com/task/multithreading/query/QueryBuildHelper.java
@@ -0,0 +1,62 @@
+package com.task.multithreading.query;
+
+import lombok.Data;
+
+import javax.persistence.NoResultException;
+import javax.persistence.Query;
+import javax.persistence.criteria.*;
+import java.util.List;
+import java.util.Optional;
+
+
+public class QueryBuildHelper {
+
+    private static final String REGEX_SYMBOL = "%";
+
+    private final CriteriaBuilder builder;
+
+    public QueryBuildHelper(CriteriaBuilder criteriaBuilder) {
+        this.builder = criteriaBuilder;
+    }
+
+    @SuppressWarnings("unchecked")
+    public <T> Optional<T> getOptionalQueryResult(Query query) {
+        try {
+            T entity = (T) query.getSingleResult();
+            return Optional.of(entity);
+        } catch (NoResultException e) {
+            return Optional.empty();
+        }
+    }
+
+    public Predicate buildAndPredicates(List<Predicate> predicates) {
+        if (predicates == null || predicates.isEmpty()) {
+            return null;
+        }
+        Predicate resultPredicate = predicates.get(0);
+        for (int i = 1; i < predicates.size(); i++) {
+            resultPredicate = builder.and(resultPredicate, predicates.get(i));
+        }
+        return resultPredicate;
+    }
+
+    public <T> Predicate buildOrEqualPredicates(Path<T> root, String columnName, List<?> values) {
+        int counter = 0;
+        Predicate predicate = null;
+        for (Object value : values) {
+            Predicate currentPredicate = builder.equal(root.get(columnName), value);
+            if (counter++ == 0) {
+                predicate = currentPredicate;
+            } else {
+                predicate = builder.or(predicate, currentPredicate);
+            }
+        }
+
+        return predicate;
+    }
+
+    public String convertToRegex(String value) {
+        return REGEX_SYMBOL + value + REGEX_SYMBOL;
+    }
+
+}
